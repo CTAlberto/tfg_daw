bc28f7851b0f0a1f5bc368e8dc414df8723f3015
CTAlberto <145429870+CTAlberto@users.noreply.github.com>
2025-04-01 19:43:30 +0200

    Borrado de clases inservibles

--- backend/src/main/java/com/tfg/backend/repository/ElementoHogarRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tfg.backend.repository;
-
-import com.tfg.backend.entity.ElementoHogar;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ElementoHogarRepository extends JpaRepository<ElementoHogar, Long>{
-}
